bb2c30b4d68884fcdfac1b5901cfaf2bdd3f4593
SkyDC <SkyDC@users.noreply.github.com>
2018-01-24 03:12:21 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    if (message.content === '!profile') {
+    	message.reply('https://r6db.com/player/a9b8c195-bbe5-4726-93c9-814da8002014/summary');
+  	}
     if (message.content === '!who') {
     	message.channel.send('@Sentient #1 :100:');
   	}
